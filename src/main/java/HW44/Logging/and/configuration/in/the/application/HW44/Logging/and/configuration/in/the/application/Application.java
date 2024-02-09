9b2e6ff482b9bfc521f99250a710e28be271cbbe
khilola.kushbakova <xilola_kushbakova@mai.ru>
2024-02-09 11:31:31 +0100

    Initial commit

---
+++ src/main/java/HW44/Logging/and/configuration/in/the/application/HW44/Logging/and/configuration/in/the/application/Application.java
@@ -0,0 +1,13 @@
+package HW44.Logging.and.configuration.in.the.application.HW44.Logging.and.configuration.in.the.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/HW44/Logging/and/configuration/in/the/application/HW44/Logging/and/configuration/in/the/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package HW44.Logging.and.configuration.in.the.application.HW44.Logging.and.configuration.in.the.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
